d721339f4c9e931de194a610085d9e2ce26e6f9e
takaerika <erika.nemoto12@gmail.com>
2025-08-10 15:54:07 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id,
+            presence: true,
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
