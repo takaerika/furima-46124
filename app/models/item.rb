29c1d53ab9d5e11c9f8d1ffbdf50505b27c8d8b2
takaerika <erika.nemoto12@gmail.com>
2025-08-11 14:29:00 +0900

    ActiveHashのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_day
+
 
   validates :image, presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id,
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
             presence: true,
+            numericality: { other_than: 0, message: "can't be blank" }
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
