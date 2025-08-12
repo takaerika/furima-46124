450ef8163414b30e4bb8a0f7223d67c9c00ac091
takaerika <erika.nemoto12@gmail.com>
2025-08-12 09:32:57 +0900

    価格表示のJavascript修正

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -1,19 +1,31 @@
-document.addEventListener('DOMContentLoaded',() => {
+const price = () => {
   const priceInput = document.getElementById('item-price');
   const taxPrice = document.getElementById('add-tax-price');
   const profit = document.getElementById('profit');
 
-  priceInput.addEventListener('input', () => {
+  if (!priceInput) return; 
+
+  const calculateAndDisplay = () => {
     const price = parseInt(priceInput.value, 10);
 
     if (!price || price < 0) {
-    taxPrice.textContent = '';
-    profit.textContent = '';
-    return;
+      taxPrice.textContent = '';
+      profit.textContent = '';
+      return;
     }
-    
+
     const fee = Math.floor(price * 0.1);
     taxPrice.textContent = fee.toLocaleString();
     profit.textContent = (price - fee).toLocaleString();
- });
-});
+  };
+
+  priceInput.addEventListener('input', calculateAndDisplay);
+
+  if (priceInput.value) {
+    calculateAndDisplay();
+  }
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
+
