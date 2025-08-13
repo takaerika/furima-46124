f1b661fec1cffe87dfe0b5422789d2053015eaf5
takaerika <erika.nemoto12@gmail.com>
2025-08-13 09:46:01 +0900

    itmesコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,20 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if user_signed_in? && @item.user == current_user
+      render :edit
+    else
+      redirect_to root_path
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
