50fd3608e3ba49d06db84cd24abd08235a4eb422
takaerika <erika.nemoto12@gmail.com>
2025-08-14 10:33:36 +0900

    ordersコントローラーにcreateアクションを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,18 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
   end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end  
 end
