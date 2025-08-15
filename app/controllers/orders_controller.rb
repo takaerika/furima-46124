d633ff12e30c6d24ba3c852a2ac758db37f4c614
takaerika <erika.nemoto12@gmail.com>
2025-08-15 17:26:21 +0900

    Rubocopの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,13 +3,11 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!
   before_action :contributor_confirmation, only: [:index, :create]
-  
-  
-  def index 
-    @order_address = OrderAddress.new
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @item = Item.find(params[:item_id])
 
+  def index
+    @order_address = OrderAddress.new
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -19,32 +17,33 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
 
-private
+  private
+
   def set_item
     @item = Item.find(params[:item_id])
   end
 
   def order_params
-  params.require(:order_address)
-        .permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number )
-        .merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+    params.require(:order_address)
+          .permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
+          .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def contributor_confirmation
     redirect_to root_path if @item.user == current_user || @item.order.present?
   end
-  
+
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: Item.find(params[:item_id]).price, 
-      card: order_params[:token],                       
-      currency: 'jpy'                   
+      amount: Item.find(params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
     )
   end
 end
